0da8f1057160d33d0e566caa3ad4e7b94e94e6b0
jon <43426006+jcwalp@users.noreply.github.com>
2018-09-26 13:46:34 -0400

    Update program12.java
    Added comments and other things

--- AP-ComputerScience-master/Classwork/program12.java
+++ AP-ComputerScience-master/Classwork/program12.java
@@ -1,20 +1,41 @@
-import java.util.Scanner;
+import java.util.Scanner; //imports scanner package
+
 public class program12 {
 
 	public static void main(String[] args) {
-		Scanner keyBoard = new Scanner(System.in);
+		Scanner keyBoard = new Scanner(System.in); //assigns scanner to keyBoard
 		
-		System.out.println("Please enter a number: ");
-		int number = keyBoard.nextInt();
+		System.out.println("Please enter a number: "); //Asks user for input
 		
-		if (number < 0){
-			System.out.println("Number is negative");
+		int number = keyBoard.nextInt(); //assigns user input to integer 
+		
+		if (number < 0){ //if user input is less than 0
+			
+			if (number % 2 == 0){  
+				System.out.println("Number is negative and even"); //prints that the number is negative
+			}
+			
+			else{
+				System.out.println("Number is negative and odd"); //prints that the number is negative
+			}
+	
 		}
 		
-		else if (number > 0){
-			System.out.println(number + " is positive");
+		else if (number == 0){
+			System.out.println(number + " is neither negative nor positive");
 		}
 		
+		else if (number > 0){ //if user input is greater than 0
+			
+			if (number % 2 == 0){  
+				System.out.println("Number is positive and even"); //prints that the number is positive and even
+			}
+			
+			else{
+				System.out.println("Number is positive and odd"); //prints that the number is positive and even
+			}
+		}
+		keyBoard.close(); //closes the keyboard
 	}
 
 }
